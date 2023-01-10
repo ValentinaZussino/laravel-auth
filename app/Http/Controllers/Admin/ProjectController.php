aec7884593ed7229d4f1262d7ae7bfcd24ad77f8
ValentinaZussino <vzussino@protonmail.com>
2023-01-10 17:30:49 +0100

    controllo funzionamento resources

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -26,7 +26,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
@@ -59,7 +59,7 @@ class ProjectController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('admin.projects.edit');
     }
 
     /**
