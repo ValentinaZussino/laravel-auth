10ef0d89e5aae836739f08054a8a4f520aff3c12
ValentinaZussino <vzussino@protonmail.com>
2023-01-12 16:26:20 +0100

    completato relazione tra types e projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,14 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'slug', 'description','dev_lang', 'framework', 'difficulty', 'team', 'git_link', 'cover_image'];
 
-    public static function generateSlug($title)
-    {
+    public static function generateSlug($title) {
+
         return Str::slug($title, '-');
+        
+    }
+
+    public function type():BelongsTo {
+
+        return $this->belongsTo(Type::class);
+
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Type extends Model
 {
@@ -14,5 +16,10 @@ class Type extends Model
     {
         return Str::slug($workflow, '-');
     }
-    
+
+    public function projects():HasMany {
+        
+        return $this->hasMany(Project::class);
+    }
+
 }
