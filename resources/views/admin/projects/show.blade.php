ba98999c72dd120bf587e3c46de3705d828f028c
ValentinaZussino <vzussino@protonmail.com>
2023-01-12 21:22:36 +0100

    agg type workflow a projects show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 
     <h1>{{$project->title}}</h1>
+    @if ($project->type)
+    <p class="fw-bold">Workflow: {{$project->type->workflow}}</p>
+    @endif
     <p>{{$project->description}}</p>
     <img src="{{ asset('storage/' . $project->cover_image) }}">
 
