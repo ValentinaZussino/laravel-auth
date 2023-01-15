019d97b49af249c4edc9ce883ae08f6be03de2c6
ValentinaZussino <vzussino@protonmail.com>
2023-01-15 20:43:04 +0100

    aggiornamento projects show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,10 +3,36 @@
 @section('content')
 
     <h1>{{$project->title}}</h1>
-    @if ($project->type)
-    <p class="fw-bold">Workflow: {{$project->type->workflow}}</p>
-    @endif
-    <p>{{$project->description}}</p>
-    <img src="{{ asset('storage/' . $project->cover_image) }}">
+
+    <div class="w-25 h-25">
+        @if($project->cover_image)
+        <img src="{{ asset('storage/' . $project->cover_image) }}">
+        @endif
+    </div>
+
+    <div class="mt-4">
+        <p> <span class="fw-bold">Linguaggi utilizzati: </span>  
+            @if (count($project->devlangs))
+                @foreach ($project->devlangs as $devlang)
+                <span>{{$devlang->name}}</span>
+                @endforeach
+            @endif
+        </p>
+       
+        @if ($project->type)
+        <p> <span class="fw-bold">Workflow: </span> {{$project->type->workflow}}</p>
+        @endif
+
+        <p> <span class="fw-bold">Link Git: </span> {{$project->git_link}}</p>
+
+        <p> <span class="fw-bold">Difficoltà: </span> {{$project->difficulty}}</p>
+
+        <p> <span class="fw-bold">Collaboratori: </span> {{$project->team}}</p>
+
+        <p><span class="fw-bold">Descrizione: </span> <br>
+            {{$project->description}}</p>
+    </div>
+    
+    
 
 @endsection
